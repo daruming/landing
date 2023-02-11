9ff9c5aae91cce016109f953272a7fa84fa1f6e2
daruming <68316976+daruming@users.noreply.github.com>
2023-02-11 18:49:23 +0900

    창업문의

--- php/contact-form.php
+++ php/contact-form.php
@@ -15,24 +15,24 @@ if (array_key_exists('email', $_POST)) {
     //Tell PHPMailer to use SMTP - requires a local mail server
     //Faster and safer than using mail()
 
-    //$mail->isSMTP();                              // Tell PHPMailer to use SMTP
+    $mail->isSMTP();                              // Tell PHPMailer to use SMTP
     
-    //$mail->Host = 'mail.yourserver.com';				  // Specify main and backup server
-    //$mail->SMTPAuth = true;                       // Enable SMTP authentication
-    //$mail->Username = 'username';             	  // SMTP username
-    //$mail->Password = 'secret';                   // SMTP password
-    //$mail->SMTPSecure = 'tls';                    // Enable encryption, 'ssl' also accepted
-    //$mail->Port = 25;                             // Set the SMTP port number - likely to be 25, 465 or 587
+    $mail->Host = 'https://daruming.github.io/landing/';				  // Specify main and backup server
+    $mail->SMTPAuth = true;                       // Enable SMTP authentication
+    $mail->Username = 'moosong';             	  // SMTP username
+    $mail->Password = 'secret';                   // SMTP password
+    $mail->SMTPSecure = 'tls';                    // Enable encryption, 'ssl' also accepted
+    $mail->Port = 25;                             // Set the SMTP port number - likely to be 25, 465 or 587
 
     //Use a fixed address in your own domain as the from address
     //**DO NOT** use the submitter's address here as it will be forgery
     //and will cause your messages to fail SPF checks
     
-    $mail->setFrom('email@your-website.com', 'your-website.com'); //**Write here sender email. For example, emails will be sent to you from your website, so write email of your website (if you don't have it, write any email, which you want) and the name of your website. Example ('email@your-website.com', 'your-website.com')) Send from a fixed, valid address in your own domain, perhaps one that allows you to easily identify that it originated on your contact form**
+    $mail->setFrom('mah1202@naver.com', 'mah1202.com'); //**Write here sender email. For example, emails will be sent to you from your website, so write email of your website (if you don't have it, write any email, which you want) and the name of your website. Example ('email@your-website.com', 'your-website.com')) Send from a fixed, valid address in your own domain, perhaps one that allows you to easily identify that it originated on your contact form**
     
     //Send the message to yourself, or whoever should receive contact for submissions
     
-    $mail->addAddress('MOOSONGLTC@GMAIL.COM'); //**WRITE HERE RECIPIENT EMAIL ADDRESS (AT THIS ADDRESS EMAILS WILL BE COME)**
+    $mail->addAddress('mah1202@naver.com'); //**WRITE HERE RECIPIENT EMAIL ADDRESS (AT THIS ADDRESS EMAILS WILL BE COME)**
     
     //Put the submitter's address in a reply-to header
     //This will fail if the address provided is invalid,
